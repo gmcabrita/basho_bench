b2b8fdd4986049f8c9bfec44076bb16965d5172f
Tyler Crain <tyler.crain@lip6.fr>
2015-04-13 16:58:22 +0200

    only works with 2 dcs for now

--- script/runMultipleTests.sh
+++ script/runMultipleTests.sh
@@ -3,6 +3,6 @@
 AllNodes=`cat script/allnodes`
 #Mode="pb"
 ./script/preciseTime.sh
-./script/runMultiDCBenchmark.sh "$AllNodes"  antidote 3 2 1 "rep1" 
-./script/runMultiDCBenchmark.sh "$AllNodes"  antidote 3 2 1 "rep2" 
-./script/runMultiDCBenchmark.sh "$AllNodes"  antidote 3 2 1 "rep3" 
+./script/runMultiDCBenchmark.sh "$AllNodes"  antidote 2 2 1 "rep1" 
+./script/runMultiDCBenchmark.sh "$AllNodes"  antidote 2 2 1 "rep2" 
+#./script/runMultiDCBenchmark.sh "$AllNodes"  antidote 3 2 1 "rep3" 
