dace52e74b4a68a24ae8025d70219c4c51357d56
Ubuntu <ubuntu@ip-172-31-7-123.us-west-1.compute.internal>
2015-12-09 23:22:32 +0000

    Fix bug in runSpeculaBench

--- masterScripts/runExprs.sh
+++ masterScripts/runExprs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./script/runSpeculaBench.sh 1 60 20 true
-./script/runSpeculaBench.sh 1 60 20 false
+./script/runSpeculaBench.sh 1 60 20 true specula_tests
+./script/runSpeculaBench.sh 1 60 20 false specula_tests
