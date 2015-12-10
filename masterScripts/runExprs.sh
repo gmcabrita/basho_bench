e5831d5d65f067fe2093af618697f276f9db7cf1
Ubuntu <ubuntu@ip-172-31-0-120.us-west-1.compute.internal>
2015-12-10 21:47:10 +0000

    Change run Expr

--- masterScripts/runExprs.sh
+++ masterScripts/runExprs.sh
@@ -2,3 +2,5 @@
 
 ./script/runSpeculaBench.sh 1 60 20 true specula_tests
 ./script/runSpeculaBench.sh 1 60 20 false specula_tests
+./script/runSpeculaBench.sh 1 100 0 true specula_tests
+./script/runSpeculaBench.sh 1 100 0 false specula_tests
