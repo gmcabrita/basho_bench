c18be0355be0fa1cb4194a9e985fedd80f89253d
Ubuntu <ubuntu@ip-172-31-15-201.us-west-1.compute.internal>
2015-12-12 19:04:16 +0000

    Change way of replacing config

--- localScripts/changeConfig.sh
+++ localScripts/changeConfig.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 File=$1
-sudo sed -i -e "s/{$2.*/{$2, $3}./" "$File"
+echo "Configuring $2 to" "$3"
+sudo sed -i -e 's/{$2.*/{$2, $3}./' "$File"
 #sudo sed -i '' -e "s/{key_range.*/key_range, 100/" "$File"
