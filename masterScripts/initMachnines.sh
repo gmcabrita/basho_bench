5ac26d4be399bf41f6504d8c3aec9d4456f2de16
Ubuntu <ubuntu@antidote00.novalocal>
2015-12-22 19:15:24 +0000

    Trying to fix bug

--- masterScripts/initMachnines.sh
+++ masterScripts/initMachnines.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+if [ $# -eq 1 ]; then
+    Clean=$1
+else
+    Clean=true
+fi
 ./script/parallel_command.sh "cd basho_bench && git stash && git pull && sudo make"
 ./script/command_to_all.sh "./basho_bench/masterScripts/config.sh" 
 ./script/command_to_all.sh "cd ./basho_bench/ && sudo chown -R ubuntu specula_tests"
+if [ "$Clean" == true ]
+then
 ./script/makeRel.sh local_specula_read
 ./script/makeRel.sh local_specula_read
+fi
 
 ./script/copy_to_all.sh ./script/allnodes ./basho_bench/script 
 ./script/command_to_all.sh "./basho_bench/masterScripts/config.sh" 
