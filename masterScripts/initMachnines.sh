fb1d43ab9f150dcd47d5d1757dcb9d58487c4171
Ubuntu <ubuntu@antidote00.novalocal>
2015-12-22 17:15:26 +0000

    ./script/

--- masterScripts/initMachnines.sh
+++ masterScripts/initMachnines.sh
@@ -2,7 +2,7 @@
 
 ./script/parallel_command.sh "cd basho_bench && git stash && git pull && sudo make"
 ./script/command_to_all.sh "./basho_bench/masterScripts/config.sh" 
-./script/command_to_all.sh "cd ./basho_bench/ && sudo rm -r ./tests && mkdir tests"
+./script/command_to_all.sh "cd ./basho_bench/ && sudo chown -R ubuntu specula_tests"
 ./script/makeRel.sh local_specula_read
 ./script/makeRel.sh local_specula_read
 
