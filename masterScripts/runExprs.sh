fd05fe699a6803d5fb442659d53b9a8d8bf898bb
Ubuntu <ubuntu@antidote00.novalocal>
2015-12-23 19:12:03 +0000

    Fix script

--- masterScripts/runExprs.sh
+++ masterScripts/runExprs.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 ## Only update slave 
-./script/runSpeculaBench.sh 1 100 0 true false 1 specula_tests
-./script/runSpeculaBench.sh 1 100 0 true false 1 specula_tests
 ./script/runSpeculaBench.sh 1 100 0 true true 1 specula_tests
-./script/runSpeculaBench.sh 1 100 0 true true 1 specula_tests
+./script/runSpeculaBench.sh 1 100 0 true false 1 specula_tests
 exit
 
 
