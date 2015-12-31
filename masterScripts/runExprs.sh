a5f033e3c34832f3461a0867f80caaed030b18f4
Ubuntu <ubuntu@ant0.novalocal>
2015-12-31 15:55:01 +0000

    Only run with more threads

--- masterScripts/runExprs.sh
+++ masterScripts/runExprs.sh
@@ -2,12 +2,8 @@
 
 ## Only update slave 
 
-./script/runSpeculaBench.sh 1 0 100 true true 1 specula_tests
-./script/runSpeculaBench.sh 1 0 100 true false 1 specula_tests
-exit
-./script/runSpeculaBench.sh 1 0 100 false false 1 specula_tests
-exit
 ./script/runSpeculaBench.sh 1 0 100 true false 5 specula_tests
+exit
 ./script/runSpeculaBench.sh 1 0 100 true false 10 specula_tests
 ./script/runSpeculaBench.sh 1 0 100 true true 1 specula_tests
 ./script/runSpeculaBench.sh 1 0 100 true true 5 specula_tests
