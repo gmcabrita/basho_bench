8b1f919590b06568913157c3dc5982e3f1983d0e
Ubuntu <ubuntu@ant0.novalocal>
2015-12-31 00:07:37 +0000

    Run 4 exprs

--- masterScripts/runExprs.sh
+++ masterScripts/runExprs.sh
@@ -2,6 +2,9 @@
 
 ## Only update slave 
 
+./script/runSpeculaBench.sh 1 0 100 false true 1 specula_tests
+./script/runSpeculaBench.sh 1 0 100 false false 1 specula_tests
+./script/runSpeculaBench.sh 1 0 100 true true 1 specula_tests
 ./script/runSpeculaBench.sh 1 0 100 true false 1 specula_tests
 exit
 ./script/runSpeculaBench.sh 1 0 100 false false 1 specula_tests
