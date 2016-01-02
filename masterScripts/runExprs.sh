cad90fd7c66cefbb91dea12f7e0424457859926f
Ubuntu <ubuntu@ant0.novalocal>
2016-01-02 19:02:59 +0000

    Change xpr

--- masterScripts/runExprs.sh
+++ masterScripts/runExprs.sh
@@ -3,7 +3,6 @@
 ## Only update slave 
 
 ./script/runSpeculaBench.sh 5 0 100 true false 1 specula_tests
-exit
 ./script/runSpeculaBench.sh 5 0 100 true false 10 specula_tests
 ./script/runSpeculaBench.sh 5 0 100 true true 1 specula_tests
 ./script/runSpeculaBench.sh 5 0 100 true true 10 specula_tests
