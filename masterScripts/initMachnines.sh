20592ae93c480ace3899c8996b9af9daad32734e
Ubuntu <ubuntu@ant0.novalocal>
2016-01-03 23:01:26 +0000

    Clean both

--- masterScripts/initMachnines.sh
+++ masterScripts/initMachnines.sh
@@ -10,12 +10,15 @@ else
 fi
 if [ $Clean == 1 ]
 then
+echo "Only cleaning antidote"
 ./script/makeRel.sh local_specula_read
 elif [ $Clean == 2 ]
+echo "Only cleaning basho_bench"
 ./script/parallel_command.sh "cd basho_bench && git stash && git pull && sudo make"
 ./script/command_to_all.sh "./basho_bench/masterScripts/config.sh" 
 ./script/command_to_all.sh "cd ./basho_bench/ && sudo chown -R ubuntu specula_tests"
 else
+echo "Cleaning both"
 ./script/makeRel.sh local_specula_read
 ./script/parallel_command.sh "cd basho_bench && git stash && git pull && sudo make"
 ./script/command_to_all.sh "./basho_bench/masterScripts/config.sh" 
