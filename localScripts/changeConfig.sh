901a18d9f53b9150a63b2f691ee3e61cb0569c24
marsleezm <mars.leezm@gmail.com>
2016-01-06 18:46:33 +0000

    Fix

--- localScripts/changeConfig.sh
+++ localScripts/changeConfig.sh
@@ -2,5 +2,16 @@
 
 File=$1
 echo "Configuring $2 to" "$3" "for" $File
+if [ $# == 3 ]
+then
+  sudo sed -i -e 's/{'"$2"'.*/{'"$2"', '"$3"'}./' "$File"
+elif [ $4 == 1 ]
+then
+sudo sed -i -e 's/{'"$2"'.*/{'"$2"', '"$3"'},/' "$File"
+elif [ $4 == 2 ]
+then
 sudo sed -i -e 's/{'"$2"'.*/{'"$2"', '"$3"'}./' "$File"
+else
+sudo sed -i -e 's/{'"$2"'.*/{'"$2"', '"$3"'}/' "$File"
+fi
 #sudo sed -i '' -e "s/{key_range.*/key_range, 100/" "$File"
