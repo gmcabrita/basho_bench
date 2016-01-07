2868baa290bd68bfb841738fa2aa72a5b1ff9534
marsleezm <mars.leezm@gmail.com>
2016-01-07 20:53:09 +0000

    Add data generate script

--- script/gatherThroughput.sh
+++ script/gatherThroughput.sh
@@ -6,10 +6,4 @@ Folder=$1
 rm $Folder/*
 ./script/parallel_fetch.sh "$AllNodes" ./basho_bench/tests/current/summary.csv $Folder
 paste $Folder/$First $Folder/$First | awk -F ',' '{print $1",", $2",", ($3-$7)",", ($4-$8)",", ($5-$9)" "}' > $1/tmp
-Files=`ls $Folder/1*`
-for F in $Files
-do
-paste $1/tmp $F | awk -F ',' '{print $1",",$2",",($3+$7)",",($4+$8)",",($5+$9)" "}' > $1/tmp1 
-mv $1/tmp1 $1/tmp
-done
-mv $1/tmp $1/specula_out
+./script/summaryThroughput.sh $Folder
---
+++ script/generateStat.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+Folder=$1
+SubFolder=`ls -d $Folder/20*`
+Output=summary
+for F in $SubFolder
+do
+    Config=`cat $F/config`
+    Committed=`cat $F/specula_out | awk -F ',' '{print $4}' | awk '{S+=$1}END{print S}'`
+    Aborted=`cat $F/specula_out | awk -F ',' '{print $5}' | awk '{S+=$1}END{print S}'`
+    ReadAborted=`cat $F/stat | awk -F ',' '{print $2}'`
+    SpeculaAborted=`cat $F/stat | awk -F ',' '{print $4}'`
+    CascadeAborted=`cat $F/stat | awk -F ',' '{print $6}'`
+    RealCommitted=$((Committed - ReadAborted - SpeculaAborted - CascadeAborted))
+    echo "$Config" $RealCommitted $SpeculaAborted $ReadAborted $CascadeAborted $Aborted >> $Output
+done
