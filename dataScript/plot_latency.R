d5b8ca970f56c7fd4ae6368bebbab94a43bdbb20
Zhongmiao Li <mars.leezm@gmail.com>
2016-01-11 12:18:22 +0000

    Modify data script

---
+++ dataScript/plot_latency.R
@@ -0,0 +1,34 @@
+plot_latency <- function(input, output, non_specula_list, specula_list)
+{
+  tc <- list()
+  ta <- list()
+  sc <- list()
+  sa <- list()
+  len <- length(first_list)
+  seq <- 1:len
+  for(i in 1:len){
+    print(first_list[i]) 
+    input_folder <- paste(input, first_list[i], sep="")
+    file <- paste(input_folder, "total_throughput", sep="/")
+    print(file)
+    mydata <- read.table(file, skip=1)
+    print(mydata)
+    tc <- c(tc, mydata[1, "V2"])
+    ta <- c(ta, mydata[1, "V7"])
+    sc <- c(sc, mydata[2, "V2"])
+    sa <- c(sa, mydata[2, "V7"])
+  }
+  tc <- as.numeric(tc)
+  ta <- as.numeric(ta)
+  sc <- as.numeric(sc)
+  sa <- as.numeric(sa)
+  print(paste("tttt",tc))
+  print(paste("scis",sc))
+  print(seq)
+  barplot(seq, tc, xlim=c(1,2), ylim=c(0,300), las=1, ylab="throughput")
+  segments(seq, tc-sc, seq, tc+sc)
+  epsilon = 0.02
+  segments(seq-epsilon,tc-sc,seq+epsilon,tc-sc)
+  segments(seq-epsilon,tc+sc,seq+epsilon,tc+sc)
+  abline(h = seq(0,250,by=50), v = 0, col = "gray60", lty=3)
+}
