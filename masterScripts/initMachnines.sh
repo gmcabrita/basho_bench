5b16ce729178916e3d3e826d5e3815ea526791b6
marsleezm <mars.leezm@gmail.com>
2016-01-14 00:59:07 +0000

    CHange back.. was stupid

--- masterScripts/initMachnines.sh
+++ masterScripts/initMachnines.sh
@@ -27,6 +27,9 @@ sudo ./script/preciseTime.sh
 sudo ./script/parallel_command.sh "sudo apt-get update && sudo apt-get -y install libwww-perl"
 sudo ./script/parallel_command.sh "cd basho_bench && git config --global user.email 'mars.leezm@gmail.com'"
 sudo ./script/parallel_command.sh "cd basho_bench && git config --global user.name 'marsleezm'"
+elif [ $Clean == 4 ]
+then
+echo "Just config!!"
 else
 echo "Cleaning all"
 sudo ./script/parallel_command.sh 'echo 127.0.0.1 `hostname` | sudo tee --append /etc/hosts'
@@ -44,11 +47,11 @@ Tpcc="./basho_bench/examples/tpcc.config"
 Load="./basho_bench/examples/load.config"
 Ant="./antidote/rel/antidote/antidote.config"
 AllNodes=`cat ./script/allnodes`
-./masterScripts/changeConfig.sh "$AllNodes" $Load concurrent 1
-./masterScripts/changeConfig.sh "$AllNodes" $Tpcc duration 1
-./masterScripts/changeConfig.sh "$AllNodes" $Load duration 1
-./masterScripts/changeConfig.sh "$AllNodes" $Tpcc to_sleep 10000
-./masterScripts/changeConfig.sh "$AllNodes" $Load to_sleep 10000
-./masterScripts/changeConfig.sh "$AllNodes" $Ant do_repl true
-./script/copy_to_all.sh ./script/allnodes ./basho_bench/script 
-./script/command_to_all.sh "./basho_bench/masterScripts/config.sh" 
+sudo ./masterScripts/changeConfig.sh "$AllNodes" $Load concurrent 1
+sudo ./masterScripts/changeConfig.sh "$AllNodes" $Tpcc duration 1
+sudo ./masterScripts/changeConfig.sh "$AllNodes" $Load duration 1
+sudo ./masterScripts/changeConfig.sh "$AllNodes" $Tpcc to_sleep 8000
+sudo ./masterScripts/changeConfig.sh "$AllNodes" $Load to_sleep 8000
+sudo ./masterScripts/changeConfig.sh "$AllNodes" $Ant do_repl true
+sudo ./script/copy_to_all.sh ./script/allnodes ./basho_bench/script 
+sudo ./script/command_to_all.sh "./basho_bench/masterScripts/config.sh" 
--- src/basho_bench_driver_load.erl
+++ src/basho_bench_driver_load.erl
@@ -85,13 +85,13 @@ new(Id) ->
     TargetNode = lists:nth((Id rem length(IPs)+1), IPs),
     true = erlang:set_cookie(node(), Cookie),
 
-    timer:sleep(ToSleep),
 
     ?INFO("Using target node ~p for worker ~p\n", [TargetNode, Id]),
     Result = net_adm:ping(TargetNode),
     ?INFO("Result of ping is ~p \n", [Result]),
     MyTxServer = {server, list_to_atom(atom_to_list(TargetNode) ++ "-cert-" ++ integer_to_list(Id))},
     {PartList, ReplList} =  rpc:call(TargetNode, hash_fun, get_hash_fun, []), %gen_server:call({global, MyTxServer}, {get_hash_fun}),
+    lager:info("Part list is ~w, Replist is ~w", [PartList, ReplList]),
     FullPartList = lists:flatten([L || {_, L} <- PartList]),
     HashLength = length(FullPartList),
     AllDcs = [N || {N, _} <- PartList],
@@ -100,6 +100,7 @@ new(Id) ->
     MyReps = lists:map(fun(Index) ->  Name = lists:nth(Index, AllDcs),  {Index, {rep, get_rep_name(TargetNode, Name)}} end, MyRepIds),
     NumDcs = length(AllDcs),
     DcId = index(TargetNode, AllDcs),
+    timer:sleep(ToSleep),
 
     lager:info("TargetNode is ~p, DcId is ~w, My Replica Ids are ~w",[TargetNode, DcId, MyReps]),
     case Id of 1 -> ets:new(meta_info, [named_table, public, set]);
