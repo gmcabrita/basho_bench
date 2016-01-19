948add347294959f867de15f6a58ccdb790541aa
marsleezm <mars.leezm@gmail.com>
2016-01-19 22:18:49 +0000

    Join nodes to ring

--- script/joinNodesToRing.sh
+++ script/joinNodesToRing.sh
@@ -13,9 +13,7 @@ else
 fi
 
 Join="sudo antidote/rel/antidote/bin/antidote-admin cluster join antidote@$First"
-Plan="sudo antidote/rel/antidote/bin/antidote-admin cluster plan"
-Commit="sudo antidote/rel/antidote/bin/antidote-admin cluster commit"
-./script/command_to_all.sh "$Others" "$Join"
-./script/command_to_all.sh "$First" "$Plan"
-./script/command_to_all.sh "$First" "$Commit"
+PlanAndCommit="sudo antidote/rel/antidote/bin/antidote-admin cluster plan && sudo antidote/rel/antidote/bin/antidote-admin cluster commit"
+./script/parallel_command.sh "$Others" "$Join"
+./script/command_to_all.sh "$First" "$PlanAndCommit"
 
