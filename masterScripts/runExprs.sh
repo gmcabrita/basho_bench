760d3daa1cd481241632fcccb984a946d9305de5
marsleezm <mars.leezm@gmail.com>
2016-02-13 01:27:52 +0000

    CHange master

--- masterScripts/runExprs.sh
+++ masterScripts/runExprs.sh
@@ -6,25 +6,25 @@ seq="1"
 threads="8"
 workloads="5 6"
 #workloads="5"
-length="1 2 4 8"
+length="1 2 4 8 16"
 warehouse="4 8"
 rep=2
 parts=3
 start_ind=1
-skip_len=21
+skip_len=18
 skipped=0
 AM=80
 AS=0
 
-th=8
-len=0
-n=0
-p=100
-wh=8
-sudo ./script/configBeforeRestart.sh $th false false $len $rep $parts
-sudo ./script/restartAndConnect.sh
-./script/runSpeculaBench.sh $th $AM $AS false false $len specula_tests $wh 0 100 $rep 0
-exit
+#th=8
+#len=0
+#n=0
+#p=100
+#wh=8
+#sudo ./script/configBeforeRestart.sh $th false false $len $rep $parts
+#sudo ./script/restartAndConnect.sh
+#./script/runSpeculaBench.sh $th $AM $AS false false $len specula_tests $wh 0 100 $rep 0
+#exit
 
 for t in $threads
 do
@@ -48,10 +48,22 @@ do
 	    fi
 	    for wh in $warehouse
 	    do
-		sudo ./script/preciseTime.sh
+            if [ $skipped -eq 1 ]
+            then
+		        sudo ./script/preciseTime.sh
+            fi
 	        for i in $seq
 	        do
 		    if [ $start_ind -gt $skip_len ]; then
+
+            if [ $skipped -eq 0 ]
+            then
+                skipped=1
+                sudo ./script/configBeforeRestart.sh $t true true $len $rep $parts
+                sudo ./script/restartAndConnect.sh
+                sleep 20
+            fi
+
 			./script/runSpeculaBench.sh $t $AM $AS true true $len specula_tests $wh $n $p $rep $start_ind
             skipped=1
 		    else
@@ -63,6 +75,8 @@ do
 	done
     done
 done
+
+exit
 
 ./script/runSpeculaBench.sh $t $AM $AS true true 1 specula_tests 4 0 0 $rep $start_ind
 start_ind=$((start_ind+1))
@@ -99,3 +113,8 @@ do
             done
         done
 done
+
+./script/runSpeculaBench.sh $t $AM $AS false false 0 specula_tests 4 0 0 $rep $start_ind
+start_ind=$((start_ind+1))
+./script/runSpeculaBench.sh $t $AM $AS false false 0 specula_tests 8 0 0 $rep $start_ind
+start_ind=$((start_ind+1))
