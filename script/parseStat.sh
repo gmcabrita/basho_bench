ffc700d48de6f8e499f5c7fd2e859ceeee0108be
marsleezm <mars.leezm@gmail.com>
2016-03-02 20:29:06 +0000

    Fix wrong script

--- script/parseStat.sh
+++ script/parseStat.sh
@@ -9,7 +9,7 @@ while [ $Good == false ]
 do
 Results=`sudo ./localScripts/getStat.sh "$1" $FetchName`
 echo "Result is " "$Results"
-#Results=`echo "$Results" | tr '\n' ' ' `
+Results=`echo "$Results" | tr '\n' ' ' `
 Results=`cut -d "[" -f 2 <<< "$Results"`
 Results=`cut -d "]" -f 1 <<< "$Results"`
 #Results=(${Results//,/ })
