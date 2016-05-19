270537ac08ea6b6f09c06dd12462e409ba16e167
marsleezm <mars.leezm@gmail.com>
2016-05-19 15:50:38 +0000

    Change num of dcs

--- masterScripts/macroBench.sh
+++ masterScripts/macroBench.sh
@@ -34,10 +34,10 @@ workloads="1 2 3 4"
 length="8"
 warehouse="2"
 
-rep=8
-parts=28
-#rep=5
-#parts=20
+#rep=8
+#parts=28
+rep=5
+parts=20
 
 start_ind=1
 skip_len=0
@@ -52,10 +52,24 @@ fast_reply=true
 
 t=8
 len=8
-sudo ./masterScripts/initMachnines.sh 1 benchmark_precise_fast_repl
+#sudo ./masterScripts/initMachnines.sh 1 benchmark_precise_fast_repl
+#sudo ./script/parallel_command.sh "cd antidote && sudo make rel"
+#sudo ./script/configBeforeRestart.sh $t $do_specula $fast_reply 8 $rep $parts $specula_read
+#sudo ./script/restartAndConnect.sh
+
+runRubis
+
+
+sudo ./masterScripts/initMachnines.sh 1 benchmark_no_specula
+specula_read=nospecula
+do_specula=false
+fast_reply=false
+len=8
 sudo ./script/parallel_command.sh "cd antidote && sudo make rel"
-sudo ./script/configBeforeRestart.sh $t $do_specula $fast_reply 8 $rep $parts $specula_read
+sudo ./script/configBeforeRestart.sh 8 $do_specula $fast_reply 0 $rep $parts $specula_read 
 sudo ./script/restartAndConnect.sh
+runRubis
+exit
 
 for t in $threads
 do
