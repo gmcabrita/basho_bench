13e7728ec61915109b9ef3ef7975d23d2f649440
marsleezm <mars.leezm@gmail.com>
2016-05-20 01:02:13 +0000

    Change fetch lat

--- script/merge_latency.sh
+++ script/merge_latency.sh
@@ -3,14 +3,16 @@
 
 Ip=`GET http://169.254.169.254/2014-11-05/meta-data/public-ipv4`
 
-cd ./tests/current/
-for l in `ls *latency`; do cat $l >> latency_bench;  done
-cd -
+#cd ./tests/current/
+#for l in `ls *latency`; do cat $l >> latency_bench;  done
+#cd -
 
 sudo ./script/getCDF.sh $Ip
 
 cd ../antidote/rel/antidote
-rm latency_ant
-for l in `ls *latency`; do cat $l >> latency_ant;  done
+rm latency_final
+rm latency_percv
+for l in `ls *final-latency`; do cat $l >> latency_final;  done
+for l in `ls *percv-latency`; do cat $l >> latency_percv;  done
 rm ./*latency
 
