53af864660ae3e4962b5f3a3337ddc539a7592f3
tcrain <tyler.crain@lip6.fr>
2016-06-02 16:28:50 +0200

    change proxy

--- script/configProxy.sh
+++ script/configProxy.sh
@@ -8,8 +8,8 @@ echo All nodes "$AllNodes"
 
 Command00="sed -i '/export http_proxy/d' ~/.bashrc"
 Command01="sed -i '/export https_proxy/d' ~/.bashrc"
-Command02="sed -i '10i export https_proxy=https://proxy.sophia.grid5000.fr:3128/' ~/.bashrc"
-Command03="sed -i '10i export http_proxy=http://proxy.sophia.grid5000.fr:3128/' ~/.bashrc"
+Command02="sed -i '10i export https_proxy=https://proxy.nancy.grid5000.fr:3128/' ~/.bashrc"
+Command03="sed -i '10i export http_proxy=http://proxy.nancy.grid5000.fr:3128/' ~/.bashrc"
 
 # echo Copying experiment keys
 # for node in $AllNodes
