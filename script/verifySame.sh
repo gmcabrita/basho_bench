25f09e0866e37a16efd776e9dd6785013182397f
marsleezm <mars.leezm@gmail.com>
2016-06-08 20:25:46 +0000

    Reduce register_item

--- script/verifySame.sh
+++ script/verifySame.sh
@@ -2,7 +2,7 @@
 set -e
 Folder=$1
 TotalTried=`cat $Folder/specula_out | awk -F ',' '{print $3}' | awk '{S+=$1}END{print S}'`
-TotalStat=`cat $Folder/stat | awk -F ' ' '{print $2" " $4" " $6 " " $8" " $10}' | awk '{t=0; for(i=1;i<=NF;i++) t+=$i; print t}' | awk '{t+=$i}END{print t}'`
+TotalStat=`cat $Folder/stat | awk -F ',' '{print $1" " $2" " $3 " " $4" " $5}' | awk '{t=0; for(i=1;i<=NF;i++) t+=$i; print t}' | awk '{t+=$i}END{print t}'`
 Diff=$((TotalTried-TotalStat))
 echo $TotalTried $TotalStat >> $Folder/diff
 if [ $Diff -lt 10 ];
