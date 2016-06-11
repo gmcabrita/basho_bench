e308a23cf5def354a5eda576c334c8267a338704
marsleezm <mars.leezm@gmail.com>
2016-06-11 16:15:44 +0000

    Done

--- script/load.sh
+++ script/load.sh
@@ -2,5 +2,5 @@
 
 cd script
 #Ip=`GET http://169.254.169.254/2014-11-05/meta-data/public-ipv4`
-sudo erl -pa script -name load@127.0.0.1 -setcookie antidote -run load load_tpcc $1 $2 $3 -run init stop
+sudo erl -pa script -name load@127.0.0.1 -setcookie antidote -noshell -run load load_tpcc $1 $2 $3 -run init stop
 cd -
