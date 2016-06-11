eafd6539b29a19f8fb79e80321b887e36888090e
marsleezm <mars.leezm@gmail.com>
2016-06-11 01:04:33 +0000

    Add rubis

--- script/parseStat.sh
+++ script/parseStat.sh
@@ -28,4 +28,21 @@ Results=`cut -d "[" -f 2 <<< "$Results"`
 Results=`cut -d "]" -f 1 <<< "$Results"`
 fi
 
+if [ $Good == false ];
+then
+Results=`sudo ./localScripts/getStat.sh "$1" $FetchName`
+echo "Result is " "$Results"
+Results=`echo "$Results" | tr '\n' ' ' `
+Results=`cut -d "[" -f 2 <<< "$Results"`
+Results=`cut -d "]" -f 1 <<< "$Results"`
+fi
+
+if [ $Good == false ];
+then
+Results=`sudo ./localScripts/getStat.sh "$1" $FetchName`
+echo "Result is " "$Results"
+Results=`echo "$Results" | tr '\n' ' ' `
+Results=`cut -d "[" -f 2 <<< "$Results"`
+Results=`cut -d "]" -f 1 <<< "$Results"`
+fi
 echo "$Results" >> $Folder/stat
