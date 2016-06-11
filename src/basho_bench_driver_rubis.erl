8c43b6b4bd1a81581549cdc20591230bff5c9f33
marsleezm <mars.leezm@gmail.com>
2016-06-11 19:03:50 +0000

    Done

--- src/basho_bench_driver_rubis.erl
+++ src/basho_bench_driver_rubis.erl
@@ -234,7 +234,7 @@ run(register_user, _KeyGen, _ValueGen, State=#state{nb_users=NBUsers, node_id=My
     ToRegisterNode = pick_node(MyNode, DcRepList, DcNoRepList, AccessMaster, AccessSlave), 
 
     %lager:info("Trying to register user ~w !!!!", [UserId]),
-    TxId = gen_server:call(TxServer, {start_tx}),
+    TxId = gen_server:call(TxServer, {start_tx, true, true}),
     Now = rubis_tool:now_nsec(),
 
     UserKey = rubis_tool:get_key({ToRegisterNode, UserId}, user),
