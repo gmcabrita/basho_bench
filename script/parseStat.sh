efd0e1a21332825dcb501276b5d54d33df9f05b3
Zhongmiao Li <mars.leezm@gmail.com>
2016-06-11 16:50:21 +0200

    Add script

--- script/parseStat.sh
+++ script/parseStat.sh
@@ -5,27 +5,19 @@ Folder=$2
 FetchName=$3
 Good=false
 
+for i in `seq 1 100`
+do
 Results=`sudo ./localScripts/getStat.sh "$1" $FetchName`
 echo "Result is " "$Results"
 Results=`echo "$Results" | tr '\n' ' ' `
 Results=`cut -d "[" -f 2 <<< "$Results"`
 Results=`cut -d "]" -f 1 <<< "$Results"`
-#Results=(${Results//,/ })
 if [[ $Results == *"Eshell"* ]]
 then
     echo "Wrong format, try again!"
-    Good=false
 else
-    Good=true
+    break
 fi
-
-if [ $Good == false ];
-then
-Results=`sudo ./localScripts/getStat.sh "$1" $FetchName`
-echo "Result is " "$Results"
-Results=`echo "$Results" | tr '\n' ' ' `
-Results=`cut -d "[" -f 2 <<< "$Results"`
-Results=`cut -d "]" -f 1 <<< "$Results"`
-fi
+done
 
 echo "$Results" >> $Folder/stat
