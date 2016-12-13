f4cac46e2aedcee94b95287766df5793412012b9
Alejandro Z. Tomsic <aletomsic@gmail.com>
2016-12-13 18:22:23 +0100

    fix scrpt worker antidote starter

--- script/FMKe/worker-start-antidote.sh
+++ script/FMKe/worker-start-antidote.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 # Call this like IP=1.2.3.4 worker-start-antidote.sh
+echo "----Worker $IP ----: cd ~/basho_bench/"
 cd ~/basho_bench/
+echo "----Worker $IP ----: git pull"
 git pull
+echo "----Worker $IP ----: cd ~/antidote
 cd ~/antidote
+echo "----Worker $IP ----: git pull"
 git pull
+echo "----Worker $IP ----: make relclean
 make relclean
+echo "----Worker $IP ----: make rel"
 make rel
-~/antidote/_build/default/rel/antidote/bin/env stop
+echo "----Worker $IP ----: INSTANCE_NAME=antidote ~/antidote/_build/default/rel/antidote/bin/env stop"
+INSTANCE_NAME=antidote ~/antidote/_build/default/rel/antidote/bin/env stop
+echo "----Worker $IP ----: IP=$IP INSTANCE_NAME=antidote ~/antidote/_build/default/rel/antidote/bin/env foreground"
 IP=$IP INSTANCE_NAME=antidote ~/antidote/_build/default/rel/antidote/bin/env foreground
 
 
