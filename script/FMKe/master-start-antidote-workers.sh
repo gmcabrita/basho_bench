84ea2849931d2192b016ed9ebd3fc57d05bb931f
Alejandro Z. Tomsic <aletomsic@gmail.com>
2016-12-13 17:06:13 +0100

    fixing ssh calls

--- script/FMKe/master-start-antidote-workers.sh
+++ script/FMKe/master-start-antidote-workers.sh
@@ -27,6 +27,33 @@ echo "Nodes are: ${Nodes}"
 for Item in ${Nodes}
 do
 
+    Command="ssh $SshOptions $USER@$Item IP=${Item} cd ~/antidote"
+    echo "Sending ssh command to ${Item}:"
+    echo "${Command}"
+    eval $Command &
+done
+
+for Item in ${Nodes}
+do
+
+    Command="ssh $SshOptions $USER@$Item IP=${Item} make relclean"
+    echo "Sending ssh command to ${Item}:"
+    echo "${Command}"
+    eval $Command &
+done
+
+for Item in ${Nodes}
+do
+
+    Command="ssh $SshOptions $USER@$Item IP=${Item} make rel"
+    echo "Sending ssh command to ${Item}:"
+    echo "${Command}"
+    eval $Command &
+done
+
+for Item in ${Nodes}
+do
+
     Command="ssh $SshOptions $USER@$Item IP=${Item} INSTANCE_NAME=antidote ~/antidote/_build/default/rel/antidote/bin/env console"
     echo "Sending ssh command to ${Item}:"
     echo "${Command}"
