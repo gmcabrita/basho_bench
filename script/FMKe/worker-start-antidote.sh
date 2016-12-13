785734ae4018a7e9e61b85298001033c7ea17c2c
Alejandro Z. Tomsic <aletomsic@gmail.com>
2016-12-13 17:27:33 +0100

    added script to launch antidote at workers

---
+++ script/FMKe/worker-start-antidote.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Call this like IP=1.2.3.4 worker-start-antidote.sh
+cd ~/basho_bench/
+git pull
+cd antidote
+git pull
+make relclean
+make rel
+IP=${IP} INSTANCE_NAME=antidote ~/antidote/_build/default/rel/antidote/bin/env foreground
+
+
+
+
