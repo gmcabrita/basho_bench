a53d3dece26b07ca1b18bb4a9d93bef24b9cffa0
marsleezm <mars.leezm@gmail.com>
2016-12-23 19:00:27 +0000

    Add max len

--- masterScripts/sigmodStress.sh
+++ masterScripts/sigmodStress.sh
@@ -143,16 +143,47 @@ clock=new
 len=0
 threads="10 20 40 80 160"
 contentions="4 3 2 1"
-folder="specula_tests/internal"
 #sudo ./masterScripts/initMachnines.sh 1 benchmark_precise_remove_stat_forward_rr 
 #sudo ./script/parallel_command.sh "cd antidote && sudo make rel"
 
+folder="specula_tests/external"
+rm -rf ./config
+echo micro duration 180 >> config
+echo micro auto_tune true >> config
+echo micro tune_period 1 >> config
+echo micro tune_sleep 1 >> config
+echo micro centralized true >> config
+echo micro max_len 9 >> config
+echo micro all_nodes replace >> config
+sudo ./script/copy_to_all.sh ./config ./basho_bench/
+sudo ./script/parallel_command.sh "cd basho_bench && sudo ./script/config_by_file.sh"
+
+#sudo ./script/configBeforeRestart.sh 500 $do_specula $len $rep $parts $specula_read
+#sudo ./script/restartAndConnect.sh
+
+for t in $threads
+do
+    sudo ./script/configBeforeRestart.sh $t $do_specula $len $rep $parts $specula_read
+    for cont in $contentions
+    do
+        if [ $cont == 1 ]; then MR=$MBIG CR=$CBIG
+        elif [ $cont == 2 ]; then MR=$MSML CR=$CBIG
+        elif [ $cont == 3 ]; then  MR=$MBIG CR=$CSML
+        elif [ $cont == 4 ]; then  MR=$MSML CR=$CSML
+        fi
+        runNTimes
+    done
+done
+
+
+folder="specula_tests/internal"
 rm -rf ./config
 echo micro duration 100 >> config
 echo micro auto_tune true >> config
-echo micro tune_period 2 >> config
+echo micro tune_period 1 >> config
 echo micro tune_sleep 1 >> config
 echo micro centralized true >> config
+echo micro max_len 1 >> config
 echo micro all_nodes replace >> config
 sudo ./script/copy_to_all.sh ./config ./basho_bench/
 sudo ./script/parallel_command.sh "cd basho_bench && sudo ./script/config_by_file.sh"
