535b0496537e34b5492f4410f86448817b027a52
Gonçalo Tomás <goncalo@goncalotomas.com>
2016-12-27 12:33:22 +0000

    change script messages, add parallelism installing software, wait for pids

--- script/FMKe/aws_build/1-setup-vms.sh
+++ script/FMKe/aws_build/1-setup-vms.sh
@@ -26,10 +26,8 @@ KEY_FILE_NAME=$(basename $PRIVATEKEY)
 
 IP_ADDR_LIST=$(echo $* | cut -d' ' -f2-)
 
-INSTALL_SOFTWARE_SCRIPT="./src/bin/install-software.sh"
-BUILD_REPOSITORIES_SCRIPT="./src/bin/clone-repositories.sh"
-REMOTE_INSTALL_SOFTWARE_SCRIPT="~/install-software.sh"
-REMOTE_BUILD_REPOSITORIES_SCRIPT="~/clone-repositories.sh"
+INSTALL_SOFTWARE_SCRIPT="./src/bin/worker-setup-machine.sh"
+REMOTE_INSTALL_SOFTWARE_SCRIPT="~/worker-setup-machine.sh"
 
 SSH_USERNAME=ubuntu
 SSH_OPTIONS="-i $PRIVATEKEY -o StrictHostKeyChecking=no"
@@ -37,23 +35,27 @@ SSH_OPTIONS="-i $PRIVATEKEY -o StrictHostKeyChecking=no"
 echo "[SCRIPT] RUNNING SCRIPT TO SETUP MULTIPLE REMOTE MACHINES..."
 
 # copy scripts to remote machines and add execute permission
-echo "[SCRIPT] 1/3: COPYING REQUIRED SCRIPTS TO REMOTE MACHINES..."
+echo "[SCRIPT] 1/2: COPYING REQUIRED SCRIPTS TO REMOTE MACHINES..."
 for IP_ADDR in $IP_ADDR_LIST; do
     scp $SSH_OPTIONS $INSTALL_SOFTWARE_SCRIPT $SSH_USERNAME@$IP_ADDR:$REMOTE_INSTALL_SOFTWARE_SCRIPT
-    scp $SSH_OPTIONS $BUILD_REPOSITORIES_SCRIPT $SSH_USERNAME@$IP_ADDR:$REMOTE_BUILD_REPOSITORIES_SCRIPT
     ssh $SSH_OPTIONS $SSH_USERNAME@$IP_ADDR chmod u+x $REMOTE_INSTALL_SOFTWARE_SCRIPT
-    ssh $SSH_OPTIONS $SSH_USERNAME@$IP_ADDR chmod u+x $REMOTE_BUILD_REPOSITORIES_SCRIPT
 done
 
 # install required software on remote machines
-echo "[SCRIPT] 2/3: INSTALLING NECESSARY SOFTWARE ON REMOTE MACHINES..."
+echo "[SCRIPT] 2/2: INSTALLING NECESSARY SOFTWARE AND REPOSITORIES ON REMOTE MACHINES..."
 for IP_ADDR in $IP_ADDR_LIST; do
-    ssh $SSH_OPTIONS $SSH_USERNAME@$IP_ADDR nohup $REMOTE_INSTALL_SOFTWARE_SCRIPT
+   ssh $SSH_OPTIONS $SSH_USERNAME@$IP_ADDR nohup $REMOTE_INSTALL_SOFTWARE_SCRIPT &
+   pids="$pids $!"
 done
 
-echo "[SCRIPT] 3/3: CLONING ALL REQUIRED REPOSITORIES TO REMOTE MACHINES..."
-for IP_ADDR in $IP_ADDR_LIST; do
-    ssh $SSH_OPTIONS $SSH_USERNAME@$IP_ADDR nohup $REMOTE_BUILD_REPOSITORIES_SCRIPT &
+echo "[SCRIPT] Waiting for SSH processes to finish their work..."
+for pid in $pids; do
+    wait $pid || let "RESULT=1"
 done
 
-echo "[SCRIPT] Done. All remote machines have the required software stack and repositories."
+if [ "$RESULT" == "1" ]; then
+    echo "[SCRIPT] Something went wrong in installing all the software!"
+    exit 1
+else
+    echo "[SCRIPT] Done. All remote machines have the required software stack and repositories."
+fi
