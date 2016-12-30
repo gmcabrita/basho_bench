2db13ef6cb3cdef889b76830527bf28eeb7e1651
Gonçalo Tomás <goncalo@goncalotomas.com>
2016-12-30 02:49:45 +0000

    use pkill, use absolute paths, fix issue with branch

--- script/FMKe/aws_build/src/bin/worker-start-fmk.sh
+++ script/FMKe/aws_build/src/bin/worker-start-fmk.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 # Call this like IP=1.2.3.4 worker-start-fmk.sh
 
-chmod +x ~/basho_bench//script/FMKe/*
-chmod +x ~/FMKe/bin/*
-chmod +x ~/antidote/bin/*
+FMK_DIR="/home/ubuntu/FMKe"
 
 if [ -z "$CLEANMAKE" ]; then
     CLEANMAKE=TRUE
 fi
 if [ -z "$GITBRANCH" ]; then
-    GITBRANCH="master"
+    GITBRANCH="perf-and-errors"
 fi
 if [ -z "$ANTIDOTE_ADDRESS" ]; then
     echo "Error: missing list of comma separated antidote IP addresses"
@@ -20,19 +18,21 @@ if [ -z "$ANTIDOTE_PB_PORT" ]; then
     exit 2
 fi
 
-
-echo "----Worker $IP ----: cd ~/FMKe/"
-cd ~/FMKe/
 if [ CLEANMAKE=TRUE ]; then
-
+    cd $FMK_DIR
+    echo "[SCRIPT] KILLING ALL ERLANG PROCESSES ON REMOTE MACHINES..."
+    pkill beam
     echo "----Worker $IP ----: git checkout $GITBRANCH"
     git checkout $GITBRANCH
     echo "----Worker $IP ----: git pull"
     git pull
+    echo "[SCRIPT] DELETING DATA FROM PREVIOUS BENCHMARKS, IF ANY..."
     echo "----Worker $IP ----: make relclean"
     make relclean
+    echo "[SCRIPT] REGENERATING RELX RELEASE..."
     echo "----Worker $IP ----: make rel"
     make rel
+
 fi
 echo "----Worker $IP ----: IP=$IP ANTIDOTE_ADDRESS=$ANTIDOTE_ADDRESS IP=$IP INSTANCE_NAME=fmk ./_build/default/rel/fmk/bin/env foreground"
 ANTIDOTE_ADDRESS=$ANTIDOTE_ADDRESS ANTIDOTE_PB_PORT=$ANTIDOTE_PB_PORT IP=$IP INSTANCE_NAME=fmk nohup ./_build/default/rel/fmk/bin/env foreground &
