ccfc8ce005aa01d8beac2a25b80074b76a31e7a3
marsleezm <mars.leezm@gmail.com>
2017-02-05 16:55:21 +0000

    Done

--- src/basho_bench_fsm_worker.erl
+++ src/basho_bench_fsm_worker.erl
@@ -599,7 +599,6 @@ worker_next_op(State) ->
             %BackoffTime = case OpTag of store_bid -> round(random:uniform()*100)*(IAbort+1); _ -> 0 end,
             random:seed(os:timestamp()),
             BackoffTime = round(random:uniform()*100)*(IAbort+1),
-            BackoffTime = 0,
             {next_state, execute, State#state{driver_state=DriverState, update_seq=UpdateSeq, store_cdf=StoreCdf,  
                     specula_txs=SpeculaTxs1, read_txs=ReadTxs2, specula_cdf=SpeculaCdf1, i_abort=IAbort+1, final_cdf=FinalCdf1}, BackoffTime};
         {wrong_msg, DriverState} ->
