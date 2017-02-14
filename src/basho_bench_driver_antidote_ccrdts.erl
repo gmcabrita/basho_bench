4e513732fa8599bf3f3c889f01c8a005a8d0bcda
Gonçalo Cabrita <_@gmcabrita.com>
2017-02-14 16:11:20 +0000

    update ccrdt bench script

--- src/basho_bench_driver_antidote_ccrdts.erl
+++ src/basho_bench_driver_antidote_ccrdts.erl
@@ -13,8 +13,8 @@
     num_keys,
     num_players,
     target,
-    added_ccrdt,
-    added_crdt
+    topkd_used_keys,
+    topkd_orset_used_keys
  }).
 
 %% ====================================================================
@@ -27,9 +27,10 @@ new(Id) ->
     Nodes = basho_bench_config:get(antidote_nodes,['antidote@127.0.0.1']),
     Cookie = basho_bench_config:get(antidote_cookie,antidote),
     NumKeys = basho_bench_config:get(num_keys, 250),
-    NumPlayers = basho_bench_config:get(num_players, 500000),
+    NumPlayers = basho_bench_config:get(num_players, 50000),
 
-    Target = lists:nth((Id rem length(Nodes)+1), IPs),
+    % Sticky "sessions"
+    Target = lists:nth((Id rem length(Nodes)+1), Nodes),
 
     %% Initialize cookie for each of the nodes
     true = erlang:set_cookie(node(), Cookie),
@@ -44,30 +45,27 @@ new(Id) ->
         num_keys = NumKeys,
         num_players = NumPlayers,
         target = Target,
-        added_ccrdt = #{},
-        added_crdt = #{}
+        topkd_used_keys = sets:new(),
+        topkd_orset_used_keys = sets:new()
       }
     }.
 
 %%% Top-K with support for deletes
 
-run(topkd_add, _KeyGen, _Value_Gen, State=#state{pid = Id, target = Target, num_keys = NumKeys, num_players = NumPlayers, added_ccrdt = Map}) ->
+run(topkd_add, _KeyGen, _Value_Gen, State=#state{pid = Id,
+                                                 target = Target,
+                                                 num_keys = NumKeys,
+                                                 num_players = NumPlayers,
+                                                 topkd_used_keys = UsedKeys}) ->
     Key = rand:uniform(NumKeys),
     PlayerId = rand:uniform(NumPlayers),
-    Score = rand:uniform(1000000000),
-    Object = {Key, antidote_ccrdt_topk_with_deletes, topk_with_dels},
+    Score = rand:uniform(250000),
+    Object = {Key, antidote_ccrdt_topk_with_deletes, topkd},
     Updates = [{Object, add, {PlayerId, Score}}],
     Response = rpc:call(Target, antidote, update_objects, [ignore, [], Updates]),
     case Response of
         {ok, _} ->
-            Map1 = case maps:is_key(Key, Map) of
-                true ->
-                    Tmp = maps:get(Key, Map),
-                    New = gb_sets:add(PlayerId, Tmp),
-                    maps:put(Key, New, Map);
-                false -> maps:put(Key, gb_sets:singleton(PlayerId), Map)
-            end,
-            {ok, State#state{added_ccrdt = Map1}};
+            {ok, State#state{topkd_used_keys = sets:add_element(Key, UsedKeys)}};
         {error,timeout} ->
             lager:info("Timeout on client ~p",[Id]),
             {error, timeout, State};
@@ -80,30 +78,37 @@ run(topkd_add, _KeyGen, _Value_Gen, State=#state{pid = Id, target = Target, num_
             {error, Reason, State}
     end;
 
-run(topkd_del, _KeyGen, _Value_Gen, State=#state{pid = Id, target = Target, added_ccrdt = Map}) ->
-    Key = case maps:keys(Map) of
-        [] -> nil;
-        ListKeys -> random_element(ListKeys)
-    end,
-    PlayerId = case maps:is_key(Key, Map) of
-        true ->
-            case gb_sets:to_list(maps:get(Key, Map)) of
-                [] -> nil;
-                ListPlayers -> random_element(ListPlayers)
-            end;
-        false -> nil
-    end,
-    case PlayerId of
-        nil -> {ok, State};
+run(topkd_del, _KeyGen, _Value_Gen, State=#state{pid = Id,
+                                                 target = Target,
+                                                 topkd_used_keys = UsedKeys}) ->
+    case sets:size(UsedKeys) of
+        0 -> {ok, State};
         _ ->
-            Object = {Key, antidote_ccrdt_topk_with_deletes, topk_with_dels},
-            Updates = [{Object, del, PlayerId}],
-            Response = rpc:call(Target, antidote, update_objects, [ignore, [], Updates]),
-            case Response of
-                {ok, _} ->
-                    Set = maps:get(Key, Map),
-                    Map1 = maps:put(Key, gb_sets:delete(PlayerId, Set), Map),
-                    {ok, State#state{added_ccrdt = Map1}};
+            Key = random_element(sets:to_list(UsedKeys)),
+            Object = {Key, antidote_ccrdt_topk_with_deletes, topkd},
+            ResponseRead = rpc:call(Target, antidote, read_objects, [ignore, [], [Object]]),
+            case ResponseRead of
+                {ok, [[]], _} ->
+                    %% in this case the top-K was empty
+                    {ok, State#state{topkd_used_keys = sets:del_element(Key, UsedKeys)}};
+                {ok, [TopK], _} ->
+                    {PlayerId, _} = random_element(TopK),
+                    Updates = [{Object, del, PlayerId}],
+                    Response = rpc:call(Target, antidote, update_objects, [ignore, [], Updates]),
+                    case Response of
+                        {ok, _} ->
+                            {ok, State};
+                        {error,timeout} ->
+                            lager:info("Timeout on client ~p",[Id]),
+                            {error, timeout, State};
+                        {error, Reason} ->
+                            lager:error("Error: ~p",[Reason]),
+                            {error, Reason, State};
+                        error ->
+                            {error, abort, State};
+                        {badrpc, Reason} ->
+                            {error, Reason, State}
+                    end;
                 {error,timeout} ->
                     lager:info("Timeout on client ~p",[Id]),
                     {error, timeout, State};
@@ -119,28 +124,21 @@ run(topkd_del, _KeyGen, _Value_Gen, State=#state{pid = Id, target = Target, adde
 
 %%%% OR-Set modeling a Top-K with removals
 
-run(or_set_topkd_add, _KeyGen, _Value_Gen, State=#state{pid = Id, target = Target, num_keys = NumKeys, num_players = NumPlayers, added_crdt = Map}) ->
+run(or_set_topkd_add, _KeyGen, _Value_Gen, State=#state{pid = Id,
+                                                        target = Target,
+                                                        num_keys = NumKeys,
+                                                        num_players = NumPlayers,
+                                                        topkd_orset_used_keys = UsedKeys}) ->
     Key = rand:uniform(NumKeys),
     PlayerId = rand:uniform(NumPlayers),
-    Score = rand:uniform(1000000000),
-    Object = {Key, antidote_crdt_orset, topk_or_set},
-    Updates = [{Object, add, {PlayerId, Score}}],
+    Score = rand:uniform(250000),
+    Object = {Key, antidote_crdt_orset, topkd_orset},
+    Element = {PlayerId, Score},
+    Updates = [{Object, add, Element}],
     Response = rpc:call(Target, antidote, update_objects, [ignore, [], Updates]),
     case Response of
         {ok, _} ->
-            Map1 = case maps:is_key(Key, Map) of
-                true ->
-                    PlayerMap = maps:get(Key, Map),
-                    NewPlayerMap = case maps:is_key(PlayerId, PlayerMap) of
-                        true ->
-                            PlayerIdScores = maps:get(PlayerId, PlayerMap),
-                            maps:put(PlayerId, [{PlayerId, Score} | PlayerIdScores], PlayerMap);
-                        false -> maps:put(PlayerId, [{PlayerId, Score}], PlayerMap)
-                    end,
-                    maps:put(Key, NewPlayerMap, Map);
-                false -> maps:put(Key, maps:from_list([{PlayerId, [{PlayerId, Score}]}]), Map)
-            end,
-            {ok, State#state{added_crdt = Map1}};
+            {ok, State#state{topkd_orset_used_keys = sets:add_element(Key, UsedKeys)}};
         {error,timeout} ->
             lager:info("Timeout on client ~p",[Id]),
             {error, timeout, State};
@@ -153,33 +151,40 @@ run(or_set_topkd_add, _KeyGen, _Value_Gen, State=#state{pid = Id, target = Targe
             {error, Reason, State}
     end;
 
-run(or_set_topkd_del, _KeyGen, _Value_Gen, State=#state{pid = Id, target = Target, added_crdt = Map}) ->
-    Key = case maps:keys(Map) of
-        [] -> nil;
-        ListKeys -> random_element(ListKeys)
-    end,
-    PlayerId = case maps:is_key(Key, Map) of
-        true ->
-            Tmp = maps:get(Key, Map),
-            case maps:keys(Tmp) of
-                [] -> nil;
-                PlayerIds -> random_element(PlayerIds)
-            end;
-        false -> nil
-    end,
-    case PlayerId of
-        nil -> {ok, State};
+run(or_set_topkd_del, _KeyGen, _Value_Gen, State=#state{pid = Id,
+                                                        target = Target,
+                                                        topkd_orset_used_keys = UsedKeys}) ->
+    case sets:size(UsedKeys) of
+        0 -> {ok, State};
         _ ->
-            PlayerMap = maps:get(Key, Map),
-            ElementsToRemove = maps:get(PlayerId, PlayerMap),
-            Object = {Key, antidote_crdt_orset, topk_or_set},
-            Updates = [{Object, remove_all, ElementsToRemove}],
-            Response = rpc:call(Target, antidote, update_objects, [ignore, [], Updates]),
-            case Response of
-                {ok, _} ->
-                    NewPlayerMap = maps:remove(PlayerId, PlayerMap),
-                    Map1 = maps:put(Key, NewPlayerMap, Map),
-                    {ok, State#state{added_crdt = Map1}};
+            Key = random_element(sets:to_list(UsedKeys)),
+            Object = {Key, antidote_crdt_orset, topkd_orset},
+            ResponseRead = rpc:call(Target, antidote, read_objects, [ignore, [], [Object]]),
+            case ResponseRead of
+                {ok, [[]], _} ->
+                    %% in this case the top-K was empty
+                    {ok, State#state{topkd_orset_used_keys = sets:del_element(Key, UsedKeys)}};
+                {ok, [TopK], _} ->
+                    {_, PlayerId} = random_element(TopK),
+                    ElementsToRemove = lists:filter(fun({I, _}) ->
+                        I == PlayerId
+                    end, TopK),
+                    Updates = [{Object, remove_all, ElementsToRemove}],
+                    Response = rpc:call(Target, antidote, update_objects, [ignore, [], Updates]),
+                    case Response of
+                        {ok, _} ->
+                            {ok, State};
+                        {error,timeout} ->
+                            lager:info("Timeout on client ~p",[Id]),
+                            {error, timeout, State};
+                        {error, Reason} ->
+                            lager:error("Error: ~p",[Reason]),
+                            {error, Reason, State};
+                        error ->
+                            {error, abort, State};
+                        {badrpc, Reason} ->
+                            {error, Reason, State}
+                    end;
                 {error,timeout} ->
                     lager:info("Timeout on client ~p",[Id]),
                     {error, timeout, State};
@@ -195,10 +200,13 @@ run(or_set_topkd_del, _KeyGen, _Value_Gen, State=#state{pid = Id, target = Targe
 
 %%%% Plain Top-K
 
-run(topk_add, _KeyGen, _Value_Gen, State=#state{pid = Id, target = Target, num_keys = NumKeys, num_players = NumPlayers}) ->
+run(topk_add, _KeyGen, _Value_Gen, State=#state{pid = Id,
+                                                target = Target,
+                                                num_keys = NumKeys,
+                                                num_players = NumPlayers}) ->
     Key = rand:uniform(NumKeys),
     PlayerId = rand:uniform(NumPlayers),
-    Score = rand:uniform(1000000000),
+    Score = rand:uniform(250000),
     Object = {Key, antidote_ccrdt_topk, topk},
     Updates = [{Object, add, {PlayerId, Score}}],
     Response = rpc:call(Target, antidote, update_objects, [ignore, [], Updates]),
@@ -219,12 +227,15 @@ run(topk_add, _KeyGen, _Value_Gen, State=#state{pid = Id, target = Target, num_k
 
 %%%% OR-Set modeling a Top-K
 
-run(or_set_topk_add, _KeyGen, _Value_Gen, State=#state{pid = Id, target = Target, num_keys = NumKeys, num_players = NumPlayers}) ->
+run(or_set_topk_add, _KeyGen, _Value_Gen, State=#state{pid = Id,
+                                                       target = Target,
+                                                       num_keys = NumKeys,
+                                                       num_players = NumPlayers}) ->
     Key = rand:uniform(NumKeys),
     PlayerId = rand:uniform(NumPlayers),
     Score = rand:uniform(1000000000),
-    Object = {Key, antidote_crdt_orset, plain_topk_orset},
-    % playerid and score are inverted here so we can get free ordering in gb_sets
+    Object = {Key, antidote_crdt_orset, topk_orset},
+    % playerid and score are inverted so we can get free ordering in gb_sets
     Element = {Score, PlayerId},
     ResponseRead = rpc:call(Target, antidote, read_objects, [ignore, [], [Object]]),
     case ResponseRead of
